28ff8bebedbaf800ec4ebd902981f4fd6e819bf1
Mohamed Reda <129882845+MohamedRedaMu@users.noreply.github.com>
2025-01-30 21:52:52 +0000

    MFPSO_algorithm.m

--- MFPSO_algorithm.m
+++ MFPSO_algorithm.m
@@ -1,5 +1,65 @@
 function [goalReached, GlobalBest, countFE] = MFPSO_algorithm()
 
+
+    %% *********************** Function Description *********************
+    %{
+    This function implements the Moth-Flame Particle Swarm Optimization algorithm.
+    Outputs:
+        goalReached: Boolean indicating if the algorithm reached the tolerance before max iterations.
+        GlobalBest: Struct containing the best solution and its cost.
+            GlobalBest.Position: the best solution
+            GlobalBest.Cost: the cost of the best found solution
+        countFE: Total number of function evaluations.
+    %}
+
+    %% *********************************** Citation **********************
+    %{
+    Citation (Please cite the following paper paper):
+        If you use this code in your research, please cite the following paper:
+        %% Bibtex:
+        @article{reda2024Optimizing,
+          title = {Optimizing the Steering of Driverless Personal Mobility Pods with a Novel Differential Harris Hawks Optimization Algorithm (DHHO) and Encoder Modeling},
+          author = {Reda, Mohamed and Onsy, Ahmed and Haikal, Amira Y and Ghanbari, Ali},
+          journal = {Sensors},
+          volume = {24},
+          number = {14},
+          pages = {4650},
+          year = {2024},
+          publisher = {MDPI},
+          doi = {https://doi.org/10.3390/s24144650},
+          url = {https://www.mdpi.com/1424-8220/24/14/4650}
+        }
+
+        %% AMA Style
+        Reda M, Onsy A, Haikal AY, Ghanbari A. Optimizing the Steering of Driverless Personal Mobility Pods with a Novel Differential Harris Hawks Optimization 
+        Algorithm (DHHO) and Encoder Modeling. Sensors. 2024; 24(14):4650. https://doi.org/10.3390/s24144650
+
+    
+        %% Chicago/Turabian Style
+        Reda, Mohamed, Ahmed Onsy, Amira Y. Haikal, and Ali Ghanbari. 2024. "Optimizing the Steering of Driverless Personal Mobility Pods with a Novel Differential
+        Harris Hawks Optimization Algorithm (DHHO) and Encoder Modeling" Sensors 24, no. 14: 4650. https://doi.org/10.3390/s24144650
+        
+    Code citation:
+        If you use this code in your research, please cite the following code as follows:
+
+        Mohamed Reda. (2024). MATLAB implementation of the Moth-Flame Particle Swarm Optimization (MFPSO) Algorithm. Available at: [https://github.com/MohamedRedaMu/MFPSO]
+
+    Contact Information:
+        For questions or further information, please contact:
+        Author Name:
+            Mohamed Reda
+    
+        Affiliation:
+            1- School of Engineering, University of Central Lancashire, Preston, PR1 2HE, UK
+            2- Computers and Control Systems Engineering Department, Faculty of Engineering, Mansoura University, Mansoura, 35516, Egypt
+    
+        Emails:
+            mohamed.reda.mu@gmail.com;
+            mohamed.reda@mans.edu.eg;
+            mramohamed@uclan.ac.uk
+
+    %}
+
    %% Begin intialiazation
     %% CEC function paramters
     fNo = 3 ; % function number
